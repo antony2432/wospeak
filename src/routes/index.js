8d7aa02fe922fd0c943fbb8464c39129aa959461
alessi <alessi_12345@hotmail.com>
2022-07-16 00:32:14 -0500

    agregando bootstrap parte 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,18 @@ import { Router } from "express";
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('formulario.ejs')
+    res.render('index.ejs')
 });
 router.get('/user', (req, res) => {
-    res.send('Alessiwooo2')
+    res.render('head.ejs')
+});
+router.get('/contacto',(req,res) =>{
+    res.render('contacto.ejs')
+});
+router.get('/main',(req,res)=>{
+    res.render('main.ejs')
+});
+router.get('/feed',(req,res)=>{
+    res.render('feed.ejs')
 })
-
 export default router;
